f39ec019f7e57e0f3266a757cb74a0e08a6162d5
slagathorr <briansuk@gmail.com>
2023-12-06 18:31:54 -0500

    versions added for terraform and gcp

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "var_gcp_project_id" {
     description = "Google Cloud project ID"
     type        = string
 }
+
+variable "var_md_svc_token" {
+    description = "MotherDuck Service Token"
+    type        = string
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    google-beta = ">= 5.8"
+  }
+}
