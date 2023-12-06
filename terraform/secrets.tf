741a8b5b449f78cfdcab52c09d17e4188647ce93
slagathorr <briansuk@gmail.com>
2023-12-06 18:44:58 -0500

    add secret version

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,10 +1,15 @@
 resource "google_secret_manager_secret" "md-svc-token" {
   secret_id = "md-svc-token"
   project  = var.var_gcp_project_id
-  
+
   replication {
     auto{}
   }
 
   depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "md-svc-token-v-1" {
+  secret      = google_secret_manager_secret.md-svc-token.id
+  secret_data = var.var_md_svc_token
 }
